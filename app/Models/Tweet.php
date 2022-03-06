2cf0772e457cfb881b2bcf55229ed8b81b1d254e
beinjamin <jauresbeinjamin@gmail.com>
2022-03-06 17:40:38 +0100

    Mise en relation user et tweet

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
     use HasFactory;
+
+    //Ici nous mettons en relation un utilisateur avec sont tweet oneToMany
+
+    public function user()
+    {
+        return $this->belongsto(user::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Tweet;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -58,4 +59,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class)->orderBy('created_at');
+    }
 }
