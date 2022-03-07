62a495ce9dc172bf14e574da41f31d3c87131896
beinjamin <jauresbeinjamin@gmail.com>
2022-03-07 11:11:20 +0100

    Update

--- database/migrations/2022_03_06_162250_create_tweets_table.php
+++ database/migrations/2022_03_06_162250_create_tweets_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
-            $table->text('context');
+            $table->text('content');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
