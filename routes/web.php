fcb6af39a015269090533e5bf147e26a1d5b1166
beinjamin <jauresbeinjamin@gmail.com>
2022-03-09 15:31:14 +0100

    Creation de la function index dans le controlleur

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('tweets', [TweetController::class, ' index'])->name('tweets.index');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
